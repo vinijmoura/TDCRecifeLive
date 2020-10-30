ec03926c8b632951f4bb6777d3b25f77d4506a0f
Vinicius Moura <v-vimou@microsoft.com>
2020-10-30 10:24:24 -0300

    Add project files.

---
+++ TDCRecifeLive/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TDCRecifeLive.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
